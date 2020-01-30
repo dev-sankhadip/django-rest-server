0041a4df789b431d102011d205c0828245fadb3d
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2020-01-30 09:43:36 +0530

    code: signup code added

--- db.py
+++ db.py
@@ -1,2 +1,14 @@
 import mysql.connector
 
+db=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root",
+    database="telecom"
+)
+
+cursor=db.cursor()
+
+class Database:
+    def signup(self, data):
+        print(data);
