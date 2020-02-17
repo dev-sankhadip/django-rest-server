b2ae10ce7d84512c67bcc02078364ba0d6e66adb
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2020-02-17 07:46:50 +0530

    some more feat added

--- title/views.py
+++ title/views.py
@@ -2,3 +2,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('hello')
+
+
+def about(request):
+    return HttpResponse('<h1>hello sankha</h1>  <a href="http://www.google.com">harry bhai</a>')
