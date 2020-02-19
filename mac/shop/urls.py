f1f19fed5ab0a877a40e158fe2147edfbdb7e340
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2020-02-19 20:34:45 +0530

    django authentication added

--- mac/shop/urls.py
+++ mac/shop/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns=[
-    path('',views.index)
+    path('',views.index),
+    path('login', views.login),
+    path('signup', views.signup),
+    path('register', views.handleSignup),
+    path('log', views.handleLogin),
+    path('logout', views.handleLogout)
 ]
