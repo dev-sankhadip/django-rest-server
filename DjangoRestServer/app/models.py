67db5c437263ebe2b6a8b3e6e40c570323b7efcf
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-04-04 20:32:30 +0530

    feat: models updated

--- DjangoRestServer/app/models.py
+++ DjangoRestServer/app/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class App(models.Model):
-    aId = models.CharField(max_legth=5, primary_key=True)
-    aName = models.CharField(max_length=True)
+    aId = models.CharField(max_length=5, primary_key=True)
+    aName = models.CharField(max_length=20)
     aDetail = models.TextField()
     aCreatedOn = models.DateTimeField(auto_now_add=True)
     aUpdatedOn = models.DateTimeField(auto_now=True)
