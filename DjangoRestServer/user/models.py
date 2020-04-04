7c950c8226759dcbc00e23622d448695b53ece63
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-04-04 10:59:29 +0530

    feat: user model added

--- DjangoRestServer/user/models.py
+++ DjangoRestServer/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    uid = models.CharField(primary_key=True, max_length=20)
+    name = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = "test"
