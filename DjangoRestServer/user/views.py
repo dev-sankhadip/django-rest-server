1c7da0bd09e6c3b005982b3601a72b9f7507396d
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-04-04 11:40:25 +0530

    feat: views code written

--- DjangoRestServer/user/views.py
+++ DjangoRestServer/user/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import status, generics
+from rest_framework.response import Response
 
-# Create your views here.
+
+class AddRetrieveUserView(generics.ListCreateAPIView):
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+    
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+
+
+class UpdateDeleteUserView(generics.RetrieveUpdateDestroyAPIView):
+
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+    
+
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
