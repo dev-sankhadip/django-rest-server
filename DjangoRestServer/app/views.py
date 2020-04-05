ef724e63d340ba4187997439a7006b96df2467bf
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-04-05 09:20:19 +0530

    feat: requirements.txt file generated

--- DjangoRestServer/app/views.py
+++ DjangoRestServer/app/views.py
@@ -1,20 +1,20 @@
 from rest_framework import status, viewsets
 from .models import App
 
-class AppViewSet(viewsets.ModelViewSet):
 
+class AppViewSet(viewsets.ModelViewSet):
 
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
-    
+
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
-    
+
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
-    
+
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
-    
+
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
