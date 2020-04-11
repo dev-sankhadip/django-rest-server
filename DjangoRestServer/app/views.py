17fd5b7ddbf87d7be1f2f3a0c7b9e0e83b736497
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-04-11 23:21:48 +0530

    feat: apps views py updated

--- DjangoRestServer/app/views.py
+++ DjangoRestServer/app/views.py
@@ -1,11 +1,14 @@
 from rest_framework import status, viewsets
+from rest_framework import status
+from rest_framework.response import Response
 from .models import App
-
+from .serializer import AddAppSerializer
 
 class AppViewSet(viewsets.ModelViewSet):
 
     def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+        serializer_class=self.get_serializer(data=request.data);
+        return Response(status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
@@ -18,3 +21,7 @@ class AppViewSet(viewsets.ModelViewSet):
 
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
+    
+    def get_serializer_class(self):
+        if self.request.method=='GET':
+            return AddAppSerializer
